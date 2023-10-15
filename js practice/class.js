bc8aad8490ac565b415856d72d789b478d0bb332
mehedi493660 <144321420+mehedi493660@users.noreply.github.com>
2023-10-15 22:24:21 +0600

    Add files via upload

---
+++ js practice/class.js
@@ -0,0 +1,32 @@
+var rahim = 500;
+var number_of_apple = 10;
+var price_of_apple = 20;
+var number_of_orrange = 8;
+var price_of_orrange = 15;
+
+var apple = number_of_apple * price_of_apple;
+var orrange = number_of_orrange * price_of_orrange;
+
+var cost = apple + orrange;
+
+var back = rahim - cost;
+
+// console.log(back);
+
+var number_of_subject = 4;
+var math = 82;
+var bangla = 75;
+var chemistry = 80;
+var physics = 87;
+
+var sum = math+bangla+chemistry+physics;
+
+var avg = sum/number_of_subject
+
+// console.log(avg)
+
+var fname = 'md mehedi';
+var lname = 'hasan rabby';
+
+var full_name = fname + ' ' + lname;
+console.log(full_name);
